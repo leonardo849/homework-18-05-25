16693e1dc4c2b72dc4df6cdca4ca912e4c1bfe47
leonardo849 <javascriptleonardolucas@gmail.com>
2025-05-18 18:44:39 -0300

    fix error

--- 2-question.py
+++ 2-question.py
@@ -2,7 +2,7 @@ age = int(input("how old are you?"))
 
 if age < 16:
     print("you can't vote")
-elif age > 18 and age <65:
+elif age >= 18 and age <65:
     print("you have to vote")
 else:
     print("you don't need to vote, but you can")
